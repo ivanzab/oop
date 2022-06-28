214eb7e8865cfe0f145483669b6e73996e08771b
Ivan Zamora <ivanzamora123.iz@gmail.com>
2022-06-28 16:40:35 -0600

    modificamos unas cositas

--- js/index.js
+++ js/index.js
@@ -1,3 +1,3 @@
-var car = new Car("AW476", new Account("Andres Herrera", "LKD231"));
+var car = new car("AW476", new Account("Andres Herrera", "LKD231"));
 car.passenger = 4;
 car.printDataCar();
--- python/main.py
+++ python/main.py
@@ -1,15 +1,9 @@
 from car import Car
+from account import Account
 
 if __name__ == "__main__":
     print("hola mundo")
 
-car = Car()
-car.license = "AMS234"
-car.driver = "Andres Herrera"
-print(vars(car))
-
-
-car2 = Car()
-car2.license = "alk123"
-car2.driver = "Matha"
-print(vars(car2))
+    car = Car("AMS234", Account("Andres Herrera", "ANDA434"))
+    print(vars(car))
+    print(vars(car.driver))
