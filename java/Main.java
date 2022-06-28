904d60a6877174d52adcbd89c6efb1abb2518ee2
Ivan Zamora <ivanzamora123.iz@gmail.com>
2022-06-28 17:23:46 -0600

    little changes to .java

--- java/Main.java
+++ java/Main.java
@@ -1,7 +1,6 @@
 class Main {
     public static void main(String[] args) {
         System.out.println("hola mundo");
-
         Car car = new Car("AMQ123", new Account("Andres Herrera", "AND324"));
         car.passenger = 4;
         car.printDataCar();
@@ -9,7 +8,6 @@ class Main {
         Car car2 = new Car("QWE567", new Account("Andrea Herrera", "ANDE213"));
         car2.passenger = 3;
         car2.printDataCar();
-        ;
 
     }
 }
