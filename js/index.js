57796499f766bb99698ef7240fd8de74227bcb7f
Ivan Zamora <ivanzamora123.iz@gmail.com>
2022-06-29 16:22:54 -0600

    muchos cambios a python

--- js/index.js
+++ js/index.js
@@ -1,3 +1,12 @@
-var car = new car("AW476", new Account("Andres Herrera", "LKD231"));
+var car = new Car("AW476", new Account("Andres Herrera", "LKD231"));
 car.passenger = 4;
 car.printDataCar();
+
+var uberX = new UberX(
+  "AW476",
+  new Account("Andres Herrera", "LKD231"),
+  "Chavrolet",
+  "Spark"
+);
+uberX.passenger = 4;
+uberX.printDataCar();
